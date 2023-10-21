7d40bd158b3805eb946e3876ef1f476fe1a5dd62
Timothy <220154856@mycput.ac.za>
2023-10-21 14:01:41 +0200

    Renamed the test package to za.ca.cput.

---
+++ demo/src/test/java/za/ac/cput/factory/LoyaltyCustomerFactoryTest.java
@@ -0,0 +1,52 @@
+package za.ac.cput.factory;
+
+import org.junit.jupiter.api.Test;
+import za.ac.cput.domain.Address;
+import za.ac.cput.domain.AddressType;
+import za.ac.cput.domain.LoyaltyCustomer;
+import za.ac.cput.factory.AddressFactory;
+import za.ac.cput.factory.LoyaltyCustomerFactory;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class LoyaltyCustomerFactoryTest {
+
+    private static Address address = AddressFactory.buildAddress("10", "Long Street", "West Side", "Sandy Shores", "California", "Georgia", "9274", AddressType.RESIDENTIAL_HOME);
+    LocalDate date = LocalDate.now();
+
+    @Test
+    public void test(){
+        LoyaltyCustomer lc = LoyaltyCustomerFactory.createLoyaltyCustomer(
+                "Sam",
+                "Doe",
+                "078-675-7850",
+                address,
+                date,
+                50.55,
+                "123456790",
+                "giveon@gmail.com"
+        );
+        System.out.print(lc.toString());
+        assertNotNull(lc);
+    }
+
+    @Test
+    public void testFail(){
+        LoyaltyCustomer lc = LoyaltyCustomerFactory.createLoyaltyCustomer(
+                "",
+                "Wise",
+                "078-675-7850",
+                address,
+                date,
+                50.55,
+                "",
+                "4");
+        System.out.print(lc.toString());
+        assertNotNull(lc);
+    }
+
+
+
+}
---
+++ demo/src/test/java/za/ac/cput/factory/PizzeriaFactoryTest.java
@@ -0,0 +1,15 @@
+package za.ac.cput.factory;
+
+import org.junit.jupiter.api.Test;
+import za.ac.cput.domain.Pizzeria;
+import za.ac.cput.factory.PizzeriaFactory;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+class PizzeriaFactoryTest {
+    @Test
+    void buildPizzaria() {
+        Pizzeria pizzeria = PizzeriaFactory.buildPizzaria("Hill Crest","Hotel Transylvania");
+        System.out.println(pizzeria.toString());
+        assertNotNull(pizzeria);
+    }
+}
